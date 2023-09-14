e59a20ad612fe4cb0688b53342b81b5a28d3329d
Jose <josemiguelcardenasfernandez10@gmail.com>
2023-09-14 00:31:57 -0400

    using postgres database for dev

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'ecommerce',
+        'PASSWORD':'holamundo',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
